2c7801334eac3e67fd4306cb12250db4c756e143
kggx <kb.gamedesign@gmail.com>
2021-06-13 08:53:57 +0200

    fixed pathes

--- config/refreshOPLibraries.py
+++ config/refreshOPLibraries.py
@@ -1,8 +1,10 @@
 import os, re
 
-base = "HGToolkit"
 
 print ('Building OPlibraries...')
+
+base = os.path.abspath("HGToolkit")
+abs_file = os.path.join(base, 'OPlibraries')
 
 for root, dirs, files in os.walk(base):
     for dir in dirs:
@@ -11,11 +13,11 @@ for root, dirs, files in os.walk(base):
                 # for each file in HGT Folders
                 if re.match("^.*(hda|hdalc|hdanc)$", file):
                     rel_path = os.path.join(dir, file).replace('\\','/')
-
-                    if rel_path not in open("HGToolkit\OPlibraries", "r").read():
+                
+                    if rel_path not in open(abs_file, "r").read():
                         print("Hit not in file")
                         
-                        with open("HGToolkit\OPlibraries", "a") as op_lib:
+                        with open(abs_file, "a") as op_lib:
                             print("Hit write.")
                             op_lib.write('\n'+ rel_path)
 
